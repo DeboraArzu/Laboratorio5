e4f0a575467388e3c59f8505f1236e80dba33490
DeboraArzu <deboranise29@hotmail.com>
2018-10-14 20:31:21 -0600

    Connection Succesful

--- lab5/routes/DB.js
+++ lab5/routes/DB.js
@@ -1,8 +1,10 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017";
+var MongoClient = require('mongodb').MongoClient, format = require('util').format;
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  console.log("Database created!");
-  db.close();
-});
+MongoClient.connect('mongodb://localhost:27017', function (err, db) {
+    if (err) {
+        throw err;
+    } else {
+        console.log("Connected");
+    }
+    db.close();
+})
