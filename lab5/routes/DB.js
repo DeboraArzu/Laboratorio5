ef8dbc50209d1f2eefc40d1aa5ebd1d00fa22d9d
DeboraArzu <deboranise29@hotmail.com>
2018-10-14 18:43:43 -0600

    MongoDB

--- lab5/routes/DB.js
+++ lab5/routes/DB.js
@@ -1,23 +1,8 @@
-var Db = require('mongodb').Db,
-    MongoClient = require('mongodb').MongoClient,
-    Server = require('mongodb').Server,
-    ReplSetServers = require('mongodb').ReplSetServers,
-    ObjectID = require('mongodb').ObjectID,
-    Binary = require('mongodb').Binary,
-    GridStore = require('mongodb').GridStore,
-    Grid = require('mongodb').Grid,
-    Code = require('mongodb').Code,
-    BSON = require('mongodb').pure().BSON,
-    assert = require('assert');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb";
 
-  // Connect using the connection string
-  MongoClient.connect("mongodb://localhost:27017/Collection", {native_parser:true}, function(err, db) {
-    assert.equal(null, err);
-
-    db.collection('Collection').update({a:1}, {b:1}, {upsert:true}, function(err, result) {
-      assert.equal(null, err);
-      assert.equal(1, result);
-
-      db.close();
-    });
-  });
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
+});
